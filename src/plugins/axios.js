a924f2e86a7e2a927dd1049d3709667059440dcf
carolluckmann <carolinaluckmann05@gmail.com>
2024-07-15 10:36:55 -0300

    Criação dos templates

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,15 +4,15 @@ import axios from 'axios'
 axios.defaults.baseURL = "http://localhost:19003/api";
 // axios.defaults.baseURL = "https://livraria-drf.herokuapp.com/api";
 
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('psg_auth_token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
+// axios.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('psg_auth_token')
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`
+//     }
+//     return config
+//   },
+//   (error) => {
+//     return Promise.reject(error)
+//   }
+// )
