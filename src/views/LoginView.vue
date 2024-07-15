f4bd5643a990ee581b48d3d5d7afd5cf9d988f1e
carolluckmann <carolinaluckmann05@gmail.com>
2024-07-15 15:05:05 -0300

    inserir passagekey

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@ import "@passageidentity/passage-elements/passage-auth";
 <template>
   <h1>Login</h1>
   <div class="authContainer">
-    <passage-auth app-id="GKj1efF61W4xT1JFyDuKPfsK"></passage-auth>
+    <passage-auth app-id="XkHUriM4JcCSuk6JvbzFZxSJ"></passage-auth>
   </div>
 </template>
 
